6a084bfc531fb075c2f41864a8cfff4a19fd3728
gchan82 <42255277+gchan82@users.noreply.github.com>
2018-11-01 21:09:14 -0700

    End of Lecture 180

---
+++ src/components/rental/RentalList.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { RentalCard } from './RentalCard';
+
+export class RentalList extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      rentals: [1, 2, 3, 4]
+    }
+  }
+
+  renderRentals() {
+    return this.state.rentals.map((rental, index) => {
+      return <RentalCard key={index} colNum='col-md-3 col-xs-6' />;
+    });
+
+  }
+
+  render() {
+    return (
+      <section id='rentalListing'>
+        <h1 className='page-title'>Your Home All Around the World</h1>
+        <div className='row'>
+          {this.renderRentals()}
+        </div>
+      </section>
+    );
+  }
+}
+
+export default RentalList;
