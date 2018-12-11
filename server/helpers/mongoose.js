0ca1ed3b505cc6135d29a0c237a622273ec2c6bf
gchan82 <42255277+gchan82@users.noreply.github.com>
2018-12-10 16:29:29 -0800

    normalizeErrors issue

--- server/helpers/mongoose.js
+++ server/helpers/mongoose.js
@@ -1,10 +1,11 @@
-module.export = {
+module.exports = {
   normalizeErrors: function (errors) {
     let normalizeErrors = [];
+    console.log('I am inside of function!!!!!!!!!!------------------------');
 
     for (let property in errors) {
       if (errors.hasOwnProperty(property)) {
-        normalizeErrors.push({title: property, detail: errors[property].message});
+        normalizeErrors.push({ title: property, detail: errors[property].message });
       }
     }
 
