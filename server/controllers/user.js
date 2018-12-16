65091c0b79c0e0ccb4e9c8ce877efcd68e566ad6
gchan82 <42255277+gchan82@users.noreply.github.com>
2018-12-16 11:06:31 -0800

    adds server, adds login and auth middleware routes on server

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,7 +1,34 @@
 const User = require('../models/user');
-const {normalizeErrors} = require('../helpers/mongoose');
+const { normalizeErrors } = require('../helpers/mongoose');
+const jwt = require('jsonwebtoken');
+const config = require('../config/dev');
 
 exports.auth = function (req, res) {
+  const { email, password } = req.body;
+
+  if (!password || !email) {
+    return res.status(422).send({ errors: [{ title: 'Data missing!', detail: 'Email or password is missing.' }] });
+  }
+
+  User.findOne({ email }, function (err, user) {
+    if (err) {
+      return res.status(422).send({ errors: normalizeErrors(err.errors) });
+    }
+    if (!user) {
+      return res.status(422).send({ errors: [{ title: 'Invalid user!', detail: 'User does not exist.' }] });
+    }
+    if (user.hasSamePassword(password)) {
+      const token = jwt.sign({
+        userId: user.id,
+        username: user.username
+      }, config.SECRET, { expiresIn: '1h' });
+
+      return res.json(token);
+
+    } else {
+      return res.status(422).send({ errors: [{ title: 'Wrong Data!', detail: 'Wrong email or password.' }] });
+    }
+  });
 };
 
 exports.register = function (req, res) {
@@ -18,25 +45,56 @@ exports.register = function (req, res) {
 
   User.findOne({ email }, function (err, existingUser) {
     if (err) {
-      return res.status(422).send({ errors: normalizeErrors(err.errors)});
+      return res.status(422).send({ errors: normalizeErrors(err.errors) });
+    }
+
+    if (existingUser) {
+      return res.status(422).send({ errors: [{ title: 'Invalid email!', detail: 'Email already in use.' }] });
+
+    }
+
+    const user = new User({
+      username,
+      email,
+      password
+    });
+
+    user.save(function (err) {
+      if (err) {
+        return res.status(422).send({ errors: normalizeErrors(err.errors) });
       }
-
-      if (existingUser) {
-        return res.status(422).send({ errors: [{ title: 'Invalid email!', detail: 'Email already in use.' }] });
-
-      }
-
-      const user = new User({
-        username,
-        email,
-        password
-      });
-
-      user.save(function (err) {
-        if (err) {
-          return res.status(422).send({ errors: normalizeErrors(err.errors) });
-        }
-        return res.json({ 'registered': true });
-      })
+      return res.json({ 'registered': true });
     })
+  })
+}
+
+exports.authMiddleware = function (req, res, next) {
+  const token = req.headers.authorization;
+
+  if (token) {
+    const user = parseToken(token);
+
+    User.findById(user.userId, function (err, user) {
+      if (err) {
+        return res.status(422).send({ errors: normalizeErrors(err.errors) });
+      }
+      if (user) {
+        res.locals.user = user;
+        next();
+      } else {
+        return notAuthorized(res);
+      }
+    })
+
+  } else {
+    return notAuthorized(res);
+  }
+}
+
+function parseToken(token) {
+  return jwt.verify(token.split(' ')[1], config.SECRET);
+}
+
+function notAuthorized(res){
+  return res.status(401).send({ errors: [{ title: 'Not authorized!', detail: 'You need to login to get access.' }] });
 }
